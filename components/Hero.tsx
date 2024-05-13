2f8419e52f5bac54d4daff70494854800289b861
Konrad Kubczyk <konrad@kubczyk.com>
2024-05-14 01:12:23 +0200

    fix: hero text color in light mode

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,8 +5,8 @@ export default function Hero() {
         <div className="hero place-items-start bg-bottom"
              style={{backgroundImage: 'url("milad-fakurian-PGdW_bHDbpI-unsplash-cropped.jpg")'}}>
             <div className="hero-overlay bg-opacity-10"></div>
-            <div className="hero-content text-primary-content w-full px-5 py-10 sm:px-10 sm:py-40">
-                <div className="w-full flex gap-8 text-primary-content">
+            <div className="hero-content w-full px-5 py-10 sm:px-10 sm:py-40">
+                <div className="w-full flex gap-8 text-neutral">
                     <ChartBarIcon className="w-24 h-24 hidden sm:block" />
                     <div>
                         <h1 className="mb-5 text-5xl font-bold">NanoMRP</h1>
