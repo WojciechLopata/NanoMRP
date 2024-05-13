63ddca59d632734550a53354ad8810c7aa58283a
Konrad Kubczyk <konrad@kubczyk.com>
2024-05-13 23:58:59 +0200

    fix: wrong colors in light mode

--- components/MPSConfig.tsx
+++ components/MPSConfig.tsx
@@ -68,13 +68,13 @@ export default function MPSConfig(props: {
                         }}/>
                 </label>
                 <label
-                    className="flex items-center border border-1 border-neutral-content/30 bg-neutral/20 py-2 px-3 rounded-lg">
+                    className="flex items-center border border-1 border-base-content/20 bg-base-100 py-2 px-3 rounded-lg">
                     <a className="text-xl pr-3">Allow adding receipts</a>
                     <input
                         type="checkbox"
                         className="toggle"
                         checked={plan.allowAddingReceipts}
-                        onChange={(e) => {
+                        onChange={() => {
                             plan.allowAddingReceipts = !plan.allowAddingReceipts
                             recalculatePlan(plan);
                         }}/>
