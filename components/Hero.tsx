08a08a609090f03482df286f01ebf22e22f7ddbe
Konrad Kubczyk <konrad@kubczyk.com>
2024-05-14 00:42:48 +0200

    feat: hero enhancements

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,19 @@
+import {ChartBarIcon} from "@heroicons/react/24/solid";
+
 export default function Hero() {
     return (
-        <div className="hero place-items-start" style={{ backgroundImage: 'url(javier-miranda-xB2XP29gn10-unsplash-cropped.jpg)' }}>
-            <div className="hero-overlay bg-opacity-25"></div>
-            <div className="hero-content text-primary-content w-full px-5 p-10 sm:px-10 sm:py-40">
-                <div className="w-full">
-                    <h1 className="mb-5 text-5xl font-bold">NanoMRP</h1>
-                    <p>
-                        Automatic MRP report calculator web app.
-                    </p>
+        <div className="hero place-items-start bg-bottom"
+             style={{backgroundImage: 'url("milad-fakurian-PGdW_bHDbpI-unsplash-cropped.jpg")'}}>
+            <div className="hero-overlay bg-opacity-10"></div>
+            <div className="hero-content text-primary-content w-full px-5 py-10 sm:px-10 sm:py-40">
+                <div className="w-full flex gap-8 text-primary-content">
+                    <ChartBarIcon className="w-24 h-24 hidden sm:block" />
+                    <div>
+                        <h1 className="mb-5 text-5xl font-bold">NanoMRP</h1>
+                        <p>
+                            Automatic MRP report calculator web app.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
