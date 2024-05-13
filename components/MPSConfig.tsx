456d46b6e319d5eb4e392c83456efc36aa1c476e
Konrad Kubczyk <konrad@kubczyk.com>
2024-05-13 23:43:53 +0200

    fix: wrong colors in light mode

--- components/MPSConfig.tsx
+++ components/MPSConfig.tsx
@@ -58,7 +58,7 @@ export default function MPSConfig(props: {
                            }}
                     />
                 </label>
-                <label className="flex items-center border border-1 border-neutral-content/30 bg-neutral/20 py-2 px-3 rounded-lg">
+                <label className="flex items-center border border-1 border-base-content/20 bg-base-100 py-2 px-3 rounded-lg">
                     <a className="text-xl pr-3">Automatic calculation</a>
                     <input
                         type="checkbox"
