a69a2be1a8c40a518b0a6ea28390ec3a313358a1
WojciechLopata <s221547@student.uek.krakow.pl>
2024-05-14 02:54:09 +0200

    feat fixes

--- lib/recalculate.ts
+++ lib/recalculate.ts
@@ -24,71 +24,62 @@ function recalculate(mrp: Plan) {
         }
 
         // Recalculate production
-        if (mrp.automaticMSPCalculations) {
-            mpsPeriod.production = 0;
-            if (mpsPeriod.available < 0) {
-                if (mrp.automaticMSPCalculations) {
-
-                    mpsPeriod.production = Math.ceil(Math.abs(mpsPeriod.available) / mrp.lotSize) * mrp.lotSize;
-                    console.log("demand", mpsPeriod.projectedDemand)
-                    console.log("Availabe", mpsPeriod.available)
-                    console.log(mpsPeriod.production)
-                    // mpsPeriod.available = Math.abs(mpsPeriod.projectedDemand - (mpsPeriod.production + Math.abs(mpsPeriod.available)))
-                    mpsPeriod.available = mpsPeriod.production + mpsPeriod.available
-                } else {
-                    console.log("test")
-                }
-            }
-        } else {
+      
             mpsPeriod.available = mpsPeriod.production + mpsPeriod.available
-        }
+        
         if (mpsPeriod.production === null || mpsPeriod.production == null) {
             mpsPeriod.production = 0
             // grossRequirements is null
         }
         componentRequired[index] = mpsPeriod.production;
+        console.log(componentRequired)
 
     });
 
-   // propagateGrossRequirementsParent(mrp, componentRequired);
+    propagateGrossRequirementsParent(mrp, componentRequired);
 
     console.log(mrp)
     return mrp;
 }
-function propagateGrossRequirements(mrp: MRPComponent, periods ) {
-    //console.log(periods)
+function propagateGrossRequirements(mrp: MRPComponent, periods) {
     mrp.children.forEach(child => {
-
         periods.forEach((period, index) => {
             if (periods[index] == null) {
-                periods[index]=0
-                // grossRequirements is null
+                periods[index] = 0;
             }
 
-        child.mrpPeriods[index].grossRequirements = periods[index]*child.quantity;
-        recalculateComponent(child);
+            // Check if automaticMRP is true before updating grossRequirements
+            if (child.mrpPeriods[index].grossRequirements < periods[index] * child.quantity) {
+                child.mrpPeriods[index].grossRequirements = periods[index] * child.quantity;
+            }
+
+            recalculateComponent(child);
+        });
     });
-})};
+}
 
 function propagateGrossRequirementsParent(mrp: Plan, periods) {
     // console.log(periods)
     mrp.mrpComponents.forEach(child => {
 
         periods.forEach((period, index) => {
-            console.log("period", period)
+           
             if (periods[index] == null) {
                 periods[index] = 0
                 // grossRequirements is null
+                
             }
-
+           if(mrp.automaticMSPCalculations){
             child.mrpPeriods[index].grossRequirements = periods[index] * child.quantity;
+           }
+
+           
             recalculateComponent(child);
         });
     })
 }
 
 export function recalculateComponent(mrp: MRPComponent, allowAddingReceipts?: boolean) {
-    console.log(mrp)
 
     let componentRequired: any[] = [];
     mrp.mrpPeriods.forEach((MRPPeriod, index) => {
