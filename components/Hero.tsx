716f66e6067210b6fb6292699cc3ca6244e7c653
RafalMalucha <s220986@student.uek.krakow.pl>
2024-05-14 13:29:44 +0200

    change hero styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,10 @@ import {ChartBarIcon} from "@heroicons/react/24/solid";
 
 export default function Hero() {
     return (
-        <div className="hero place-items-start bg-bottom"
+        <div className="hero h-[25vh] place-items-start bg-bottom"
              style={{backgroundImage: 'url("milad-fakurian-PGdW_bHDbpI-unsplash-cropped.jpg")'}}>
             <div className="hero-overlay bg-opacity-10"></div>
-            <div className="hero-content w-full px-5 py-10 sm:px-10 sm:py-40">
+            <div className="hero-content w-full px-5 py-10 sm:px-10 sm:py-12">
                 <div className="w-full flex gap-8 text-neutral">
                     <ChartBarIcon className="w-24 h-24 hidden sm:block" />
                     <div>
